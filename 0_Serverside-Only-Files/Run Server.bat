85ad6a3a2f7638433b5dfabdc20b0a6a46d54746
NukelordPvP <63741267+NukelordPvP@users.noreply.github.com>
2024-02-28 00:35:10 -0800

    updated resources and gas mod
    updated resources and gas mod

--- 0_Serverside-Only-Files/Run Server.bat
+++ 0_Serverside-Only-Files/Run Server.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Java\jre1.8.0_351\bin\java.exe" -Xmx4096M -Xms4096M -jar custom.jar -nogui
+"C:\Program Files\Java\jdk1.8.0_321\bin\java.exe" -Xmx4096M -Xms4096M -jar custom.jar -nogui
 pause
